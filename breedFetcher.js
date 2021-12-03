2a096b4e893b5251555ecfdaa2911899adb90d8d
Michael <mikechansas@gmail.com>
2021-12-03 01:30:02 +0000

    Add testing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,13 +1,13 @@
 const request = require('request');
 
-const fetchBreedDescription = function (breedName, callback) {
+const fetchBreedDescription = function(breedName, callback) {
   request(`http://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     if (error) {
       callback(error);
     } else {
       const data = JSON.parse(body);
-      callback(null, ((data.length > 0) ? data[0]['description'] : 'Breed not found')); //Code supplied by mentor
+      callback(null, ((data.length > 0) ? data[0]['description'] : 'Breed not found.')); //Code supplied by mentor
     }
   });
 };
