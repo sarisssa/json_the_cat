cb71dc293ec79fdad254ca49a11131854a021a63
Michael <mikechansas@gmail.com>
2021-12-03 01:11:19 +0000

    Refactor wit index.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,15 @@
-const arr = process.argv.splice(2);
 const request = require('request');
 
-const breed = arr[0];
+const fetchBreedDescription = function (breedName, callback) {
+  request(`http://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-request(`http://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-    if (error) return console.log(error); 
-    
-    let data = JSON.parse(body);
-
-    if (!data.length) console.log('This breed does not exist');
-    else {
-      let description = data[0]['description'];
-      console.log(description);
+    if (error) {
+      callback(error);
+    } else {
+      const data = JSON.parse(body);
+      callback(null, ((data.length > 0) ? data[0]['description'] : 'Breed not found')); //Code supplied by mentor
     }
-});
+  });
+};
+
+module.exports = fetchBreedDescription;
